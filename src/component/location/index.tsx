0c5f2bc51b839d6cfcff006d162b75dabf1e88e4
gnsdlok-lang <gnsdlok@gmail.com>
2025-12-18 23:32:33 +0900

    내 청첩장 완성!

---
+++ src/component/location/index.tsx
@@ -0,0 +1,59 @@
+import { Map } from "./map"
+import CarIcon from "../../icons/car-icon.svg?react"
+import BusIcon from "../../icons/bus-icon.svg?react"
+import { LazyDiv } from "../lazyDiv"
+import { LOCATION, LOCATION_ADDRESS } from "../../const"
+
+export const Location = () => {
+  return (
+    <>
+      {/* 웨딩홀 이름 + 주소 + 지도 */}
+      <LazyDiv className="card location">
+        <h2 className="english">Location</h2>
+        <div className="addr">
+          {LOCATION}
+          <div className="detail">{LOCATION_ADDRESS}</div>
+        </div>
+        <Map />
+      </LazyDiv>
+
+      {/* 교통 안내 카드 */}
+      <LazyDiv className="card location">
+        {/* 대중교통 - 버스 */}
+        <div className="location-info">
+          <div className="transportation-icon-wrapper">
+            <BusIcon className="transportation-icon" />
+          </div>
+          <div className="heading">대중교통</div>
+          <div />
+          <div className="content">
+            <b>버스 이용</b>
+            <br />
+            11, 213, 601, 706번 버스 이용
+            <br />
+            <b>법원 등기국(아이파크시티 방면)</b> 하차
+          </div>
+        </div>
+
+        {/* 자가용 */}
+        <div className="location-info">
+          <div className="transportation-icon-wrapper">
+            <CarIcon className="transportation-icon" />
+          </div>
+          <div className="heading">자가용</div>
+          <div />
+          <div className="content">
+            네이버 지도, 카카오 네비, 티맵 등 이용
+            <br />
+            <b>라도무스 아트센터 웨딩홀</b> 검색
+            <br />
+            <br />
+            <b>- 주차 요금은 무료입니다</b>
+            <br />
+            (주차장 이용 시 웨딩홀과 바로 연결)
+          </div>
+        </div>
+      </LazyDiv>
+    </>
+  )
+}
---
+++ src/const.ts
@@ -0,0 +1,90 @@
+import dayjs from "dayjs"
+import utc from "dayjs/plugin/utc"
+import timezone from "dayjs/plugin/timezone"
+import "dayjs/locale/ko"
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.locale("ko")
+
+export { dayjs }
+
+export const WEDDING_DATE = dayjs.tz("2026-05-03 11:00", "Asia/Seoul")
+export const WEDDING_DATE_FORMAT = `YYYY년 MMMM D일 dddd A h시${WEDDING_DATE.minute() === 0 ? "" : " m분"}`
+
+// 예식 당월 휴무일. 켈린더에 표시하기 위함.
+// 예: 예식일 8월 -> 8월 15일 광복절
+export const HOLIDAYS = [5,25]
+
+export const LOCATION = "라도무스 아트센터 웨딩홀"
+export const LOCATION_ADDRESS = "대전광역시 유성구 동서대로 639"
+
+// 카카오톡 공유 시 위치 정보로 사용할 주소.
+// LOCATION 과 동일하게 설정해도 무방하나, 필요에 따라 좀 더 상세히 작성 가능.
+export const SHARE_ADDRESS = LOCATION
+export const SHARE_ADDRESS_TITLE = LOCATION
+
+// 네이버 지도 및 카카오 네비게이션에 사용할 좌표. [경도, 위도] 형식.
+export const WEDDING_HALL_POSITION = [127.333591, 36.333002]
+
+// 네이버 지도의 웨딩홀 장소 ID
+// 네이버 지도 웹페이지에서 웨딩홀 검색 후 URL에서 확인 가능.
+// 예: https://map.naver.com/p/entry/place/13321741 -> 13321741
+export const NMAP_PLACE_ID = 37638309
+
+// 카카오 지도의 웨딩홀 장소 ID
+// 카카오 지도 웹페이지에서 웨딩홀 검색 후 해당 장소에서 상세보기 클릭 시 URL에서 확인 가능.
+// 예: https://place.map.kakao.com/8634826 -> 8634826
+export const KMAP_PLACE_ID = 27596552
+
+export const BRIDE_FULLNAME = "신수빈"
+export const BRIDE_FIRSTNAME = "수빈"
+export const BRIDE_TITLE = "딸"
+export const BRIDE_FATHER = "신경선"
+export const BRIDE_MOTHER = "허란정"
+export const BRIDE_INFO = [
+  {
+    relation: "신부",
+    name: BRIDE_FULLNAME,
+    phone: "010-8745-3371",
+    account: "우리은행 0000000000000",
+  },
+  {
+    relation: "신부 아버지",
+    name: BRIDE_FATHER,
+    phone: "010-7735-3371",
+    account: "하나은행 00000000000",
+  },
+  {
+    relation: "신부 어머니",
+    name: BRIDE_MOTHER,
+    phone: "010-0000-0000",
+    account: "하나은행 00000000000000",
+  },
+]
+
+export const GROOM_FULLNAME = "김세훈"
+export const GROOM_FIRSTNAME = "세훈"
+export const GROOM_TITLE = "아들"
+export const GROOM_FATHER = "김종태"
+export const GROOM_MOTHER = "추교순"
+export const GROOM_INFO = [
+  {
+    relation: "신랑",
+    name: GROOM_FULLNAME,
+    phone: "010-7355-1502",
+    account: "농협 3020981723161",
+  },
+  {
+    relation: "신랑 아버지",
+    name: GROOM_FATHER,
+    phone: "010-3484-1502",
+    account: "신한은행 000000000000",
+  },
+  {
+    relation: "신랑 어머니",
+    name: GROOM_MOTHER,
+    phone: "010-9802-1502",
+    account: "국민은행 000000000000",
+  },
+]
