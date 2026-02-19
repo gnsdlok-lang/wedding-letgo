2454c7c4812492f27a2ecbae83089fb2ffb14f92
gnsdlok-lang <gnsdlok@gmail.com>
2026-02-19 12:21:48 +0900

    사진 용량 줄이고 멘트 추가

--- src/component/information/index.tsx
+++ src/component/information/index.tsx
@@ -29,11 +29,11 @@ export const Information2 = () => {
       <div className="info-card">
         <div className="label">마음 전하기</div>
         <div className="content">
-          참석이 어려워 직접 축하해주지 못하는
+          멀리서도 잊지 않고
           <br />
-          분들을 위해 계좌번호를 기재하였습니다.
+          보내주신 축복과 따뜻한 마음
           <br />
-          넓은 마음으로 양해 부탁드립니다.
+          잊지 않고 잘 살겠습니다.
         </div>
 
         <div className="break" />
